52ef66de2f74e7673f0c4b35665829f960964480
문석영(Seokyoung Moon)/Biz플랫폼개발팀/SKP <seokyoung.moon90@gmail.com>
2021-07-06 16:38:31 +0900

    :zap: 3-1 Two Sum
    Map으로 속도향상

--- richestCustomerWealth.js
+++ richestCustomerWealth.js
@@ -2,14 +2,27 @@
  * @param {number[][]} accounts
  * @return {number}
  */
- var maximumWealth = function(accounts) {
-  let maxWealth = 0;
-  accounts.forEach(account => {
-      let sum = account.reduce((sum, wealth) => sum += wealth);
-      if(sum > maxWealth) {
-          maxWealth = sum;
-      }
-  })
+//  var maximumWealth = function(accounts) {
+//   let maxWealth = 0;
+//   accounts.forEach(account => {
+//       let sum = account.reduce((sum, wealth) => sum += wealth);
+//       if(sum > maxWealth) {
+//           maxWealth = sum;
+//       }
+//   })
  
-  return maxWealth;
+//   return maxWealth;
+// };
+
+var twoSum = function(nums, target) {
+  let map = new Map();
+  
+  for(let i = 0; i < nums.length; i ++) {
+      if(map.has(target - nums[i])) {
+          return [map.get(target - nums[i]), i];
+      } else {
+          map.set(nums[i], i);
+      }
+  }
+return [];
 };
