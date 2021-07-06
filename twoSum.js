c7f2e94d872311e425e013a6ddbc1276a5ec4b64
문석영(Seokyoung Moon)/Biz플랫폼개발팀/SKP <seokyoung.moon90@gmail.com>
2021-07-06 16:47:11 +0900

    :adhesive_bandage: 길 잃은 코드 수정

--- twoSum.js
+++ twoSum.js
@@ -1,12 +1,25 @@
+// var twoSum = function(nums, target) {
+//   let result = null;
+//   for(let i = 0; i < nums.length; i++) {
+//       for(let j = i+1; j < nums.length; j++) {
+//           if(nums[i] + nums[j] === target) {
+//               result = [i, j];
+//           }
+//       }
+//       if(!!result) break;
+//   }
+//   return result;
+// };
+
 var twoSum = function(nums, target) {
-  let result = null;
-  for(let i = 0; i < nums.length; i++) {
-      for(let j = i+1; j < nums.length; j++) {
-          if(nums[i] + nums[j] === target) {
-              result = [i, j];
-          }
+  let map = new Map();
+  
+  for(let i = 0; i < nums.length; i ++) {
+      if(map.has(target - nums[i])) {
+          return [map.get(target - nums[i]), i];
+      } else {
+          map.set(nums[i], i);
       }
-      if(!!result) break;
   }
-  return result;
+return [];
 };
